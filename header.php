9bcd139ffb4ebe7d1c5168c811c2d39b24e3ab2b
Carlos Miguel <carlos.lopez.ag@gmail.com>
2022-09-20 19:32:52 +0100

    primera carga

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header.css">
+    <script defer src="header.js" type="module"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div id="encabezado">
+        <img id="logo" src="imagenes/logo.jpg" alt="Logo">
+        <h1>Repositorio de Imagenes</h1>
+        <span id="carga">Cargar Fotos</span>
+        <article>        
+            <label for="busqueda">Busqueda por Palabras Clave:</label>
+            <input type="text" id="busqueda">
+        </article>
+    </div>
+</body>
+</html>
